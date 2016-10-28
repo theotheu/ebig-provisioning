7e58fb3fe2aacdaad9aa0a3b4dbb660478d6c46c
Theo Theunissen <theo.theunissen@gmail.com>
2016-10-28 19:23:37 +0200

    update for local playbook

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ cd ebig-provisioning/ansible
 
 #ansible-playbook setup.yml --ask-sudo-pass --module-path ./ansible --limit "$(hostname)"
 #ansible-playbook -i "localhost" setup.yml
-ansible-playbook -i "localhost" local.yml
+ansible-playbook local.yml  --connection=local
 exit 0
