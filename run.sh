d264ac9511fdf06100e1b52222f286918be720a1
Theo Theunissen <theo.theunissen@gmail.com>
2016-10-29 10:56:34 +0200

    updates for local install

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install ansible
 # Create user non-interactively
 # ---> Proxmox template 16.04 is complaining about missing users <---
 # Replace ___PASSWORD___ and ___USERNAME___ with actual values
-# useradd -m -p `perl -e 'printf("%s\n", crypt($ARGV[0], "___PASSWORD___"))' "moeilijkwachtwoord"` -s /bin/bash ___USERNAME___
+# useradd -m -p `perl -e 'printf("%s\n", crypt($ARGV[0], "password"))' "___PASSWORD___"` -s /bin/bash ___USERNAME___
 
 cd ~
 mkdir -p code
